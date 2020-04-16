c1bacb21a077a0d6f7dbd299fe4d2a5c8d49c0fa
gabrielknoxx1 <60486860+gabrielknoxx1@users.noreply.github.com>
2020-04-15 21:37:47 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  
+  return response.json(repositories);
+ 
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url , techs , likes} = request.body;
+  
+  const repositorie = { id: uuid(), title, url , techs, likes};
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
+
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const {id} = request.params;
+  const { title, url , techs } = request.body;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Repositorie not found' })
+  }
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositorieIndex] = repositorie;
+  
+  return response.json(repositorie);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Repositorie not found' })
+  }
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send();
+});
+
+
+app.post("/repositories/:id/like", (request, response) => {
+  const {id} = request.params;
+  
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Repositorie not found' })
+  }
+
+  repositorie = repositories[repositorieIndex] ;
+
+  repositorie.likes = repositorie.likes + 1 ;
+  
+  return response.json(repositorie);
+});
+
+
+
+module.exports = app;
